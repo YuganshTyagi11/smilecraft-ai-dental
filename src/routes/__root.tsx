5feae6dc07cd34473b72bf37e728e7b8c4562fb9
Yuganshconversely <yugansh@conversely.in>
2026-06-10 20:18:02 +0530

    add favicon SVG logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -84,6 +84,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@SmileCraft" },
     ],
     links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
